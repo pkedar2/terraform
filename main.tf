c68adc47c1f391b276fbf770c701eb71a8e2d3a9
pkedar2 <pranavkedar18@gmail.com>
2025-06-09 12:28:43 +0530

    terraform ec2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
 
   tags = {
