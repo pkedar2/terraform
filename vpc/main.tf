6fb4937add63e80674a2a32047db36f64bf7e784
pkedar2 <pranavkedar18@gmail.com>
2025-06-09 16:20:33 +0530

    vpc and subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,1 +1,54 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+resource "aws_vpc" "test" {
+    cidr_block       = "10.0.0.0/16"
 
+  tags = {
+    Name = "test-vpc"
+  }
+  
+}
+# Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.test.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.test.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "PublicSubnet2"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "PrivateSubnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "PrivateSubnet2"
+  }
+}
