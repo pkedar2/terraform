3be2067e82a74a20c4eefa1b796f8d93597eb41d
Pranav Kedar <158163280+pkedar2@users.noreply.github.com>
2025-06-10 19:53:26 +0530

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -82,25 +82,3 @@ resource "aws_route_table_association" "public_assoc_2" {
   subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route.id
 }
-
-resource "aws_route_table" "private_route" {
-  vpc_id = aws_vpc.test.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "PrivateRouteTable"
-  }
-}
-resource "aws_route_table_association" "private_assoc_1" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_route.id
-}
-
-resource "aws_route_table_association" "private_assoc_2" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_route.id
-}
